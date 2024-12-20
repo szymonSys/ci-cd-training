64472a297a9228f93ead0def50740214acb0ea41
Szymon Sysło <s.syslo@piwik.pro>
2024-12-20 15:07:14 +0100

    Fix worklogs and actions, add footer with version info

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  console.log(import.meta.env);
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
       </div>
       <Outlet />
+      <footer>
+        <div className="flex relative">
+          {import.meta.env.VITE_ENVIRONMENT !== 'PROD' && (
+            <div className="self-center absolute right-0 bg-white rounded-lg shadow-sm p-2">
+              ENV: {import.meta.env.VITE_ENVIRONMENT ?? 'Local'} - VER: {__APP_VERSION__}
+            </div>
+          )}
+        </div>
+      </footer>
     </div>
   );
 };
