dead90b6ae8c54415b8e572e48bdc4fc9c2a3312
Szymon Sysło <s.syslo@piwik.pro>
2024-12-20 15:09:13 +0100

    Add version log (#10)
    * Add version log
    
    * Fix worklogs and actions, add footer with version info

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,15 @@ const App = () => {
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
       </div>
       <Outlet />
+      <footer>
+        <div className="flex relative">
+          {import.meta.env.VITE_ENVIRONMENT !== 'PROD' && (
+            <div className="self-center absolute right-0 bg-white rounded-lg shadow-sm p-2">
+              ENV: {import.meta.env.VITE_ENVIRONMENT ?? 'Local'} - VER: {__APP_VERSION__}
+            </div>
+          )}
+        </div>
+      </footer>
     </div>
   );
 };
